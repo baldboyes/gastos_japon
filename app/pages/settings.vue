114f2f34857e03fef94e4251ba585bfdd00672ad
baldboyes <david@baldboy.es>
2025-11-12 01:50:52 +0100

    paginacion5

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -183,7 +183,7 @@
         </Card>
 
         <div class="text-center text-sm text-gray-400">
-          <span>Versión</span> <span class="font-medium">2.5.2</span>
+          <span>Versión</span> <span class="font-medium">2.5.3</span>
         </div>
       </div>
     </div>
